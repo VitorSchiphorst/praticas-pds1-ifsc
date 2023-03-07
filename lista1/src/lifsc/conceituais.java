df1f8c73f401588c89333bf899ae9162a2baa26f
Vitor Hugo Schiphorst <vitorhugo.schiphorst@gmail.com>
2023-03-07 12:06:37 -0300

    Lista 01 - Atualizado

--- lista1/src/lifsc/conceituais.java
+++ lista1/src/lifsc/conceituais.java
@@ -2,7 +2,7 @@
 
 // Resposta 1 -> São coleções de recursos utilizados por programas no computador
 
-// Resposta 2 -> É uma lingugagem utilizada para criar interfaces gráficas
+// Resposta 2 -> É uma biblioteca utilizada para criar interfaces gráficas
 
 // Resposta 3 -> A: JButton
 			//	 B: JtextFieldName
@@ -10,3 +10,13 @@
 			//	 D: JProgressBar
 			//	 E: JRadioButton
 			//	 F: JComboBox
+
+// Resposta 4 -> O propósito do Layout é algo mais interativo e apresentável para o usuário. Tipos de Layout BorderLayout, BoxLayout, FlowLayout e GridLayout
+
+// Resposta 5 -> Usamos o "If" para a validação de entrada de dados com uma condição
+
+// Resposta 6 -> Swing = Interface gráfica, componentes evoluídos e trabalhar com conceitos de acessibilidade
+			//	 AWT = Interface gráfica, componentes básicos e interação direta com o sistema de janela local
+			//   JavaFX = Interface gráfica, componentes, Audio e Vídeo
+
+// Resposta 7 -> O "Look" é o olhar, no caso, o visual, sendo a aparência e componentes exibidos. Já o "Feel", como já diz é sentir, no caso, a função 
--- lista1/src/lifsc/pratica2.java
+++ lista1/src/lifsc/pratica2.java
@@ -1,12 +1,22 @@
 package lifsc;
 
+import java.util.ArrayList;
+
 import javax.swing.JOptionPane;
 
 public class pratica2 {
 
 	public static void main(String[] args) {
-		<ArrayList> elementos = 
-
+		ArrayList<String> elementos = new ArrayList<String>();
+		
+		for (int i = 0; i < 10; i++) {
+			elementos.add(JOptionPane.showInputDialog("Informe o elemento: " + elementos));
+		}
+		
+		for (int i = 0; i < args.length; i++) {
+			JOptionPane.showMessageDialog(null, "Elemento inserido" + elementos);
+		}
+		
 	}
 
 }
